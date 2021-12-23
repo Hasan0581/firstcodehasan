93f81320d1ba72ebce63860216486d324832435b
hasan0581 <hasanhussain0581@gmail.com>
2021-12-23 21:14:53 +0530

    UC-3

---
+++ UC-3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+ispartTime=1
+isFullTime=2
+empRateperHr=20
+randomCheck=$((RANDOM%3))
+
+if [ $isFullTime -eq $randomCheck ]
+then 
+      empHrs=8;
+elif [ $ispartTime  -eq $randomCheck ]
+then
+      empHrs=4;
+else
+      empHrs=0;
+fi
+
+salary=$(($empHrs*$empRateperHr));
+  
